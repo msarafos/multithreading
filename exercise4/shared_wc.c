c1d6c3c2c308b2b4dc8885bc4b6835b2842b6922
msaraf <62781124+msarafos@users.noreply.github.com>
2021-12-31 00:21:07 +0200

    Update shared_wc.c

--- exercise4/shared_wc.c
+++ exercise4/shared_wc.c
@@ -13,7 +13,7 @@ typedef struct shared {
     pthread_cond_t  women;
     pthread_cond_t  isDone;
     int people;
-	int capacity;
+    int capacity;
     int menw, womenw;        // men and women waiting in queue
     int men_in, women_in;    // men and women inside WC (CS)
     int type;                // men or women
@@ -24,20 +24,20 @@ void entry_woman(void* args){
 
     pthread_mutex_lock(&arguments->monitor);
     while (1) {
-		if(arguments->men_in > 0 || arguments->women_in == arguments->capacity){    
-       		arguments->womenw++;
-       		pthread_cond_wait(&arguments->women,&arguments->monitor);
+	if(arguments->men_in > 0 || arguments->women_in == arguments->capacity){    
+       	    arguments->womenw++;
+       	    pthread_cond_wait(&arguments->women,&arguments->monitor);
     	}
     	if((arguments->women_in < arguments->capacity) && (arguments->men_in == 0)) {
-    		arguments->women_in++;
-			break;
+    	    arguments->women_in++;
+            break;
     	}
-		else {
-			pthread_cond_wait(&arguments->women,&arguments->monitor);
-		}
+	else {
+	    pthread_cond_wait(&arguments->women,&arguments->monitor);
 	}
+    }
     pthread_mutex_unlock(&arguments->monitor);
-	return;
+    return;
 }
 
 void exit_woman(void* args){
@@ -45,49 +45,50 @@ void exit_woman(void* args){
 
     pthread_mutex_lock(&arguments->monitor);
     arguments->women_in--;
-	arguments->people--;
+    arguments->people--;
 	    
     if (arguments->women_in == 0) {
-		if (arguments->womenw > 0) {
-			arguments->womenw--;
-			pthread_cond_signal(&arguments->women);
-		}
-		if (arguments->menw > 0) {
-       		arguments->menw--;
-        	pthread_cond_signal(&arguments->men);
-    	}
+	if (arguments->womenw > 0) {
+	    arguments->womenw--;
+	    pthread_cond_signal(&arguments->women);
 	}
+	if (arguments->menw > 0) {
+       	    arguments->menw--;
+            pthread_cond_signal(&arguments->men);
+    	}
+    }
     pthread_cond_signal(&arguments->women);
+    
     /* Last one to inform main. */
     if(arguments->men_in == 0 && arguments->menw == 0 && arguments->women_in == 0 && arguments->womenw == 0){
         pthread_cond_signal(&arguments->isDone);
         pthread_mutex_unlock(&arguments->monitor);
         return;    
     }
-	pthread_cond_signal(&arguments->isDone);
+    pthread_cond_signal(&arguments->isDone);
     pthread_mutex_unlock(&arguments->monitor);
-	return;
+    return;
 }
 
 void entry_man(void*args){
     shared* arguments = (void*)args;
     
     pthread_mutex_lock(&arguments->monitor);
-	while (1) {
+    while (1) {
     	if ((arguments->women_in > 0) || (arguments->men_in == arguments->capacity)) {
-        	arguments->menw++;
-        	pthread_cond_wait(&arguments->men,&arguments->monitor);
+            arguments->menw++;
+            pthread_cond_wait(&arguments->men,&arguments->monitor);
     	}
     	if((arguments->men_in < arguments->capacity) && (arguments->women_in == 0)) {
-        	arguments->men_in++;
-			break;
+            arguments->men_in++;
+	    break;
     	}
-		else {
-        	pthread_cond_wait(&arguments->men,&arguments->monitor);
-		}
+	else {
+            pthread_cond_wait(&arguments->men,&arguments->monitor);
 	}
+    }
     pthread_mutex_unlock(&arguments->monitor);
-	return;
+    return;
 }
 
 
@@ -96,11 +97,11 @@ void exit_man(void* args){
 
     pthread_mutex_lock(&arguments->monitor);
     arguments->men_in--;
-	arguments->people--;
+    arguments->people--;
 
-	if (arguments->men_in == 0) {
+    if (arguments->men_in == 0) {
     	if (arguments->womenw > 0) {
-        	arguments->womenw--;
+            arguments->womenw--;
             pthread_cond_signal(&arguments->women);
         }
         if (arguments->menw > 0) {
@@ -108,7 +109,7 @@ void exit_man(void* args){
             pthread_cond_signal(&arguments->men);
        	}
     }
-	pthread_cond_signal(&arguments->men);
+    pthread_cond_signal(&arguments->men);
     
     /* Last one to inform main. */
     if(arguments->men_in == 0 && arguments->menw == 0 && arguments->women_in == 0 && arguments->womenw == 0){
@@ -116,9 +117,9 @@ void exit_man(void* args){
         pthread_mutex_unlock(&arguments->monitor);
         return;    
     }
-	pthread_cond_signal(&arguments->isDone); 
-    pthread_mutex_unlock(&arguments->monitor);
-	return;
+    pthread_cond_signal(&arguments->isDone); 
+    pthread_mutex_unlock(&arguments->monitor);  
+    return;
 }
 
 
@@ -158,9 +159,9 @@ int main(int argc,char* argv[]){
         exit(EXIT_FAILURE);
     }
     printf("[*] Initializing structures and variables.\n");
-	sleep(2);
+    sleep(2);
  
-	info.people = 0; 
+    info.people = 0; 
     info.capacity = atoi(argv[1]);
     pthread_mutex_init(&info.monitor,NULL);
     pthread_cond_init(&info.men,NULL);
@@ -177,24 +178,24 @@ int main(int argc,char* argv[]){
     while (scanf("%d:%d:%d", &num_of_people, &info.type, &delay) != EOF) {
         pthread_t people_threads[num_of_people];
         info.people += num_of_people;
-		for (i = 0; i < num_of_people; i++) {
+	for (i = 0; i < num_of_people; i++) {
             pthread_create(&people_threads[i], NULL, people_fun, (void *) &info);
         }
         sleep(delay);
     }
    	
-	/* Main waiting to get signaled for termination. */ 
-	while (info.people > 0)
-		pthread_cond_wait(&info.isDone,&info.monitor); 
+    /* Main waiting to get signaled for termination. */ 
+    while (info.people > 0)
+        pthread_cond_wait(&info.isDone,&info.monitor); 
     
-	printf("[*] Destroying condition variables and monitor and exiting.\n");
+    printf("[*] Destroying condition variables and monitor and exiting.\n");
     sleep(2);
     pthread_mutex_destroy(&info.monitor);
     pthread_cond_destroy(&info.men);
     pthread_cond_destroy(&info.women);
     pthread_cond_destroy(&info.isDone);
     
-	printf("[*] Done.\n");
-	sleep(2);
+    printf("[*] Done.\n");
+    sleep(2);
     return 0;
 }
