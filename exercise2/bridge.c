9c84970b5de8cca7fc877b2fb90485b32d2c9f32
msaraf <62781124+msarafos@users.noreply.github.com>
2021-12-31 00:03:14 +0200

    Update bridge.c

--- exercise2/bridge.c
+++ exercise2/bridge.c
@@ -12,13 +12,11 @@ typedef struct bridge {
     pthread_cond_t  blue;
     pthread_cond_t  red;
     pthread_cond_t  isDone;
-    int vehicles;
-	int rcars_passed;
-	int bcars_passed;
-	int capacity;
-    int bluew, redw;        // blue and red cars waiting in queue.
-    int blue_in, red_in;    // blue and red on bridge. (CS)
-    int type;               // blue or red.
+    int             vehicles;
+    int             capacity;
+    int             bluew, redw;        // blue and red cars waiting in queue.
+    int             blue_in, red_in;    // blue and red on bridge. (CS)
+    int             type;               // blue or red.
 } bridge;
 
 void entry_red(void* args) {
@@ -26,17 +24,17 @@ void entry_red(void* args) {
 
     pthread_mutex_lock(&arguments->monitor);
 	
-	arguments->redw++;
-	while (arguments->blue_in > 0 || arguments->red_in == arguments->capacity){    
+    arguments->redw++;
+    while (arguments->blue_in > 0 || arguments->red_in == arguments->capacity) {    
        	pthread_cond_wait(&arguments->red, &arguments->monitor);
     }
-	arguments->red_in++;
-	arguments->redw--;
+    arguments->red_in++;
+    arguments->redw--;
     if(arguments->red_in < arguments->capacity) {
     	pthread_cond_signal(&arguments->red);
     }
     pthread_mutex_unlock(&arguments->monitor);
-	return;
+    return;
 }
 
 void exit_red(void* args) {
@@ -44,20 +42,20 @@ void exit_red(void* args) {
 
     pthread_mutex_lock(&arguments->monitor);
     arguments->red_in--;
-	arguments->vehicles--;
+    arguments->vehicles--;
 	    
     if (arguments->red_in == 0 && arguments->bluew > 0 ) 
-		pthread_cond_signal(&arguments->blue);
-	else if (arguments->red_in == 0 && arguments->redw > 0)
+	pthread_cond_signal(&arguments->blue);
+    else if (arguments->red_in == 0 && arguments->redw > 0)
         pthread_cond_signal(&arguments->red);
     
-	/* Last one to inform main. */
+    /* Last one to inform main. */
     if(arguments->blue_in == 0 && arguments->bluew == 0 && arguments->red_in == 0 && arguments->redw == 0){
         pthread_cond_signal(&arguments->isDone);
         pthread_mutex_unlock(&arguments->monitor);
         return;    
     }
-	pthread_cond_signal(&arguments->isDone);
+    pthread_cond_signal(&arguments->isDone);
     pthread_mutex_unlock(&arguments->monitor);
 	return;
 }
@@ -67,18 +65,17 @@ void entry_blue(void*args) {
     
     pthread_mutex_lock(&arguments->monitor);
 
-	arguments->bluew++;
+    arguments->bluew++;
     while ((arguments->red_in > 0) || (arguments->blue_in == arguments->capacity)) {
         pthread_cond_wait(&arguments->blue, &arguments->monitor);
     }
-	arguments->blue_in++;
-	arguments->bluew--;
+    arguments->blue_in++;
+    arguments->bluew--;
     if((arguments->blue_in < arguments->capacity) /*&& (arguments->red_in == 0)*/) {
         pthread_cond_signal(&arguments->blue);
     }
-    //pthread_cond_wait(&arguments->blue, &arguments->monitor);
     pthread_mutex_unlock(&arguments->monitor);
-	return;
+    return;
 }
 
 
@@ -87,11 +84,11 @@ void exit_blue(void* args){
 
     pthread_mutex_lock(&arguments->monitor);
     arguments->blue_in--;
-	arguments->vehicles--;
+    arguments->vehicles--;
 
-   	 if (arguments->red_in == 0 && arguments->bluew > 0 )
+    if (arguments->red_in == 0 && arguments->bluew > 0 )
          pthread_cond_signal(&arguments->blue);
-     else if (arguments->red_in == 0 && arguments->redw > 0)
+    else if (arguments->red_in == 0 && arguments->redw > 0)
          pthread_cond_signal(&arguments->red);
  
     /* Last one to inform main. */
@@ -100,9 +97,9 @@ void exit_blue(void* args){
         pthread_mutex_unlock(&arguments->monitor);
         return;    
     }
-	pthread_cond_signal(&arguments->isDone); 
+    pthread_cond_signal(&arguments->isDone); 
     pthread_mutex_unlock(&arguments->monitor);
-	return;
+    return;
 }
 
 
@@ -142,45 +139,42 @@ int main(int argc,char* argv[]){
         exit(EXIT_FAILURE);
     }
     printf("[*] Initializing structures and variables.\n");
-	sleep(2);
+    sleep(2);
  
-	info.vehicles = 0; 
+    info.vehicles = 0; 
     info.capacity = atoi(argv[1]);
     pthread_mutex_init(&info.monitor, NULL);
     pthread_cond_init(&info.blue, NULL);
     pthread_cond_init(&info.red, NULL);
     pthread_cond_init(&info.isDone, NULL);
-    info.rcars_passed = 0;
-	info.bcars_passed = 0;
-	info.red_in = 0;
+    info.red_in = 0;
     info.blue_in = 0;
     info.bluew = 0;
     info.redw = 0;
-    
     
     printf("[*] Reading from stdin and creating threads.\n");
     sleep(2);	
     while (scanf("%d:%d:%d", &num_of_vehicles, &info.type, &delay) != EOF) {
         pthread_t vehicle_threads[num_of_vehicles];
         info.vehicles += num_of_vehicles;
-		for (i = 0; i < num_of_vehicles; i++) {
+	for (i = 0; i < num_of_vehicles; i++) {
             pthread_create(&vehicle_threads[i], NULL, vehicle_fun, (void *) &info);
         }
         sleep(delay);
     }
    	
-	/* Main waiting to get signaled for termination. */ 
-	while (info.vehicles > 0)
-		pthread_cond_wait(&info.isDone, &info.monitor); 
+    /* Main waiting to get signaled for termination. */ 
+    while (info.vehicles > 0)
+	pthread_cond_wait(&info.isDone, &info.monitor); 
     
-	printf("[*] Destroying condition variables and monitor and exiting.\n");
+    printf("[*] Destroying condition variables and monitor and exiting.\n");
     sleep(2);
     pthread_mutex_destroy(&info.monitor);
     pthread_cond_destroy(&info.blue);
     pthread_cond_destroy(&info.red);
     pthread_cond_destroy(&info.isDone);
     
-	printf("[*] Done.\n");
-	sleep(2);
+    printf("[*] Done.\n");
+    sleep(2);
     return 0;
 }
